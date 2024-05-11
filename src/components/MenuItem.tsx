d1b1b64f0782098dca8fea87ab04136854a1fae0
Your Name <my@email.org>
2024-05-12 00:17:06 +0100

    testingApplication2

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,7 +6,7 @@ type Props = {
   addToCart: () => void;
 };
 
-const MenuItems = ({ menuItem, addToCart }: Props) => {
+const MenuItem = ({ menuItem, addToCart }: Props) => {
   return (
     <Card className="cursor-pointer" onClick={addToCart}>
       <CardHeader>
@@ -19,4 +19,4 @@ const MenuItems = ({ menuItem, addToCart }: Props) => {
   );
 };
 
-export default MenuItems;
+export default MenuItem;
